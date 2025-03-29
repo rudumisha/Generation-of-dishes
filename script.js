85b0ddab599fe10eb59d5c44d1e04d36eb9605ee
Sasha Lytvyn <litvinalex918@gmail.com>
2025-03-29 17:58:31 +0200

    додали змінм

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("name").value = localStorage.getItem("name") || "";
-    document.getElementById("email").value = localStorage.getItem("email") || "";
-
-    document.getElementById("registrationForm").addEventListener("input", function() {
-        localStorage.setItem("name", document.getElementById("name").value);
-        localStorage.setItem("email", document.getElementById("email").value);
-    });
-});
